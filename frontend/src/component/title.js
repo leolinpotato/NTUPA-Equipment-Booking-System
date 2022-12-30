b5c3c49bb101bba5b8cb9135c26f25af11f56421
lijiacian <lijiacian@gmail.com>
2022-12-30 17:13:12 +0800

    add title

---
+++ frontend/src/component/title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/title.css';
+
+const Title = () => {
+	return (
+		<div classname = 'titleContainer'>
+			<a href="/">
+                <div className='title'>
+                    <h1>PA器材租借系統</h1>
+                </div>
+            </a>
+		</div>
+	)
+}
+export default Title;
