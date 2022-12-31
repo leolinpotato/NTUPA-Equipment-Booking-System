362761baa2480dc84493075c76588abc4a53876d
lijiacian <lijiacian@gmail.com>
2022-12-31 22:14:17 +0800

    add sideBar.js

--- frontend/src/container/sideBar.js
+++ frontend/src/container/sideBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from "react";
+import '../css/sideBar.css';
 //import Filter from './filter';
 //import { MdTune, MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
