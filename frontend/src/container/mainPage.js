0ca888dad346de000e7f8486780a7f454eac4fcb
lijiacian <lijiacian@gmail.com>
2022-12-31 22:33:33 +0800

    add mainPage.css, update sideBar.css

--- frontend/src/container/mainPage.js
+++ frontend/src/container/mainPage.js
@@ -1,10 +1,10 @@
 import React from 'react'
-//import '../css/mainPage.css'
+import '../css/mainPage.css'
 
 const MainPage = () => {
     return (
         <div className='mainPageContainer'>
-            <h2>What do you want to do?</h2>
+            <h2 className='mainPage'>What do you want to do?</h2>
         </div>
     )
 }
