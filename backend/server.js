5c09a26b3f11783dae8f407c26fceca4398d27b7
lijiacian <lijiacian@gmail.com>
2023-01-05 14:00:43 +0800

    deploy test

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,14 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
+if(process.env.NODE_ENV === "production"){
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function(req, res){
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 4001;
 
 app.listen(port, () => {
