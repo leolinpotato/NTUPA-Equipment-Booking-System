e571bf2fc4ce357915fde8420cf3088872c963dc
leolin <leolin0904@gmail.com>
2023-01-05 17:47:27 +0800

    update mainPage

--- frontend/src/container/mainPage.js
+++ frontend/src/container/mainPage.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import '../css/mainPage.css'
-import Duck from "../css/duckduck.png"
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 const MainPage = () => {
+
+    const navigate = useNavigate(); 
+    const ToActivity = () => {
+        navigate('/borrow');
+    }
+
+    useEffect(() => {
+        ToActivity();
+    }, []);
+
     return (
-        <div className='mainPageContainer'>
-            <h2 className='mainPage'>What do you want to do?</h2>
-            <img src= { Duck } className='duckduck'/>
-        </div>
+        <></>
     )
 }
 export default MainPage
